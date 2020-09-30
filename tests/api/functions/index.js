a808c52bd85b66ef8ba1beed821c2bde7332f5c4
Nikita Lebedev <y87cgp@gmail.com>
2020-09-30 13:16:00 +0300

    Исправил тесты

--- tests/api/functions/index.js
+++ tests/api/functions/index.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 
 const createPostFunc = (url) => {
-  const port = 30303;
+  const port = 30001;
   return async (obj) => {
     return axios.post(`http://localhost:${port}/api/${url}`, obj);
   }
 };
 
 const get = async (url) => {
-  const port = 30303;
+  const port = 30001;
   return axios.get(`http://localhost:${port}/api/${url}`);
 }
 
