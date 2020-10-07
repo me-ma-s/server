5fb6ce420e8443b906474a78ac8248108d2c3364
lebedev-nikita <y87cgp@gmail.com>
2020-10-07 15:23:21 +0300

    починил logOut

--- src/middlewares/requestHandlers/logOut.js
+++ src/middlewares/requestHandlers/logOut.js
@@ -2,7 +2,7 @@ const handleError = require('./handleError');
 
 async function logOut(req, res) {
   try {
-    res.clearCookie(user_id);
+    res.clearCookie('user_id');
     res.sendStatus(200);
 
   } catch (err) {
